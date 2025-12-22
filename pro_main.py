2c209df5605e4a16bb9a0f42862a2d0626f3ec71
Mithun Kannaa <mithunkannaa609@gmail.com>
2025-12-22 16:42:32 +0530

    cnn architecture completed

--- pro_main.py
+++ pro_main.py
@@ -1,4 +1,3 @@
-
 from datasets import load_dataset
 import torch
 import torch.nn as nn
@@ -8,6 +7,25 @@ import time
 ds=load_dataset("Hemg/AI-Generated-vs-Real-Images-Datasets",
                 split='train')
 
+class CNNModel(nn.Module):
+    def __init__(self,input_param):
+        super(CNNModel,self).__init__()
+        self.conv_layer=nn.Sequential(
+            nn.Conv2d(in_channels=3,out_channels=32,kernel_size=3,stride=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3,stride=2)
+        )
+        self.dense_layer=nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(_,128),
+            nn.LeakyReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(128,2)
+        )
+        
+model=CNNModel(_)
 
 def gen(batch_size=32):
     X=[]
@@ -20,6 +38,5 @@ def gen(batch_size=32):
             X,y=[],[]
     
 
-model=nn.Sequential()
 
 
